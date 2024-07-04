dafa8c247aea6b41ba347128b17eeab218421c08
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2024-07-04 23:02:10 +0200

    "Removed 'server' from .gitignore"

---
+++ internal/grpc/server/server.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/iRankHub/backend/internal/grpc/proto"
+	"github.com/iRankHub/backend/internal/models"
+)
+
+func StartGRPCServer(queries *models.Queries) error {
+	// Create a new gRPC server
+	grpcServer := grpc.NewServer()
+
+	// Create the AuthServer
+	authServer, err := NewAuthServer(queries)
+	if err != nil {
+		return fmt.Errorf("failed to create AuthServer: %v", err)
+	}
+
+	// Register the AuthService with the gRPC server
+	proto.RegisterAuthServiceServer(grpcServer, authServer)
+
+	// Start the gRPC server on a specific port
+	grpcPort := "0.0.0.0:8080"  // Changed to listen on all interfaces
+	listener, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		return fmt.Errorf("failed to listen on port %s: %v", grpcPort, err)
+	}
+	log.Printf("gRPC server started on %s", grpcPort)
+	return grpcServer.Serve(listener)
+}
