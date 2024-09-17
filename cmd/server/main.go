a8180adbef8a00c5c0ec3e49a504846a45f10b25
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2024-09-17 15:00:52 +0200

    Refactor main.go to use app.StartBackend() function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,78 +1,9 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/spf13/viper"
-
-	"github.com/iRankHub/backend/envoy"
-	"github.com/iRankHub/backend/internal/config"
-	"github.com/iRankHub/backend/internal/grpc/server"
-	"github.com/iRankHub/backend/internal/utils"
+	"github.com/iRankHub/backend/cmd/app"
 )
 
 func main() {
-	// Load the database configuration using Viper
-	viper.SetConfigFile(".env")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("Failed to read config file: %v", err)
-	}
-
-	dbConfig := &config.DatabaseConfig{
-		Host:     viper.GetString("DB_HOST"),
-		Port:     viper.GetInt("DB_PORT"),
-		User:     viper.GetString("DB_USER"),
-		Password: viper.GetString("DB_PASSWORD"),
-		Name:     viper.GetString("DB_NAME"),
-		Ssl: 	  viper.GetString("DB_SSL"),
-	}
-
-	// Connect to the database
-	connString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
-		dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name, dbConfig.Ssl)
-	db, err := sql.Open("pgx", connString)
-	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
-	}
-	log.Println("Successfully connected to the database")
-	defer db.Close()
-
-	// Run database migrations
-	migrationPath := "file://internal/database/postgres/migrations"
-	m, err := migrate.New(migrationPath, connString)
-	if err != nil {
-		log.Fatalf("Failed to create migrate instance: %v", err)
-	}
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatalf("Failed to run database migrations: %v", err)
-	}
-	log.Println("Successfully ran database migrations")
-
-	// Initialize the token configuration
-	err = utils.InitializeTokenConfig()
-	if err != nil {
-		log.Fatalf("Failed to initialize token configuration: %v", err)
-	}
-
-	// Start the token cleanup goroutine
-	utils.StartTokenCleanup()
-
-	// Start the Envoy Proxy server
-	go func() {
-		if err := envoy.StartEnvoyProxy(); err != nil {
-			log.Fatalf("Failed to start Envoy Proxy: %v", err)
-		}
-	}()
-
-	// Start the gRPC server
-	if err := server.StartGRPCServer(db); err != nil {
-		log.Fatalf("Failed to start gRPC server: %v", err)
-	}
+	app.StartBackend()
 }
